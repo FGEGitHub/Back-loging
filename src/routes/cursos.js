498ae3a5d75016275e61092446a8311c836e7619
Pipo <pipao.pipo@gmail.com>
2023-02-13 23:01:46 -0300

    inscripcion a curso

--- src/routes/cursos.js
+++ src/routes/cursos.js
@@ -1,72 +1,103 @@
 const express = require('express')
 const router = express.Router()
-const { isLoggedIn,isLoggedInn, isLoggedInn2} = require('../lib/auth') //proteger profile
+const { isLoggedIn, isLoggedInn, isLoggedInn2 } = require('../lib/auth') //proteger profile
 const pool = require('../database')
 
 
 
-router.get('/lista/',isLoggedInn2, async (req, res) => {
-   
-  
-     const etc = await pool.query ('select * from cursos' )
+router.get('/lista/', isLoggedInn, async (req, res) => {
 
-   res.json(etc);
- //res.render('index')
- })
 
- 
- router.get('/detalledelcurso/:id',isLoggedInn2, async (req, res) => {
-   const id = req.params.id
+  const etc = await pool.query('select * from cursos')
+
+  res.json(etc);
+  //res.render('index')
+})
+
+
+/////// lista desde el usuario 1
+router.get('/listaniv1/:usuario', isLoggedInn, async (req, res) => {
+  const usuario = req.params.usuario
+
+  const etc = await pool.query('select cursos.id,cursos.fecha , encargado, nombre, cupo, cursos.id, cursado.inscripcion from cursos left join cursado on cursos.id=(select cursado.id_curso from cursado where  id_usuario =?)', [usuario])
+  console.log(etc)
+  res.json(etc);
+  //res.render('index')
+})
+
+router.get('/detalledelcurso/:id', isLoggedInn2, async (req, res) => {
+  const id = req.params.id
   try {
-    const etc = await pool.query ('select * from clases where id_curso = ? ',[id] )
+    const etc = await pool.query('select * from clases where id_curso = ? ', [id])
 
     res.json(etc);
   } catch (error) {
     res.json(['']);
   }
- 
-//res.render('index')
+
+  //res.render('index')
 })
- router.post("/crear", isLoggedInn2,async (req, res) => {
-  const { nombre, observaciones, encargado,cupo } = req.body
+router.post("/crear", isLoggedInn2, async (req, res) => {
+  const { nombre, observaciones, encargado, cupo } = req.body
 
 
-    try {
-      datos = {
-        fecha: (new Date(Date.now())).toLocaleDateString(),
-        nombre,
-        observaciones, 
-        encargado,
-        cupo
-      }
-    
-      await pool.query('insert cursos  set ?',   [datos] )
-      aux = await pool.query('select * from cursos ')
-      console.log((aux.length))
-      console.log(aux[(aux.length-1)])
-      console.log(aux[(aux.length-1)]['id'])
-      idaux = aux[(aux.length-1)]['id']
-      nove = {
-            id_ref: idaux,
-            asunto:'Nuevo Curso',
-            detalle:'Lanzamiento de curso:'+nombre,
-            fecha:(new Date(Date.now())).toLocaleDateString(),
-
-
-      }
-      await pool.query('insert novedades  set ?',   [nove] )
-      res.json('Realizado')
-    } catch (error) {
-      console.log(error)
-      res.json('Error algo sucedio')
+  try {
+    datos = {
+      fecha: (new Date(Date.now())).toLocaleDateString(),
+      nombre,
+      observaciones,
+      encargado,
+      cupo
     }
- 
- })
+
+    await pool.query('insert cursos  set ?', [datos])
+    aux = await pool.query('select * from cursos ')
+    console.log((aux.length))
+    console.log(aux[(aux.length - 1)])
+    console.log(aux[(aux.length - 1)]['id'])
+    idaux = aux[(aux.length - 1)]['id']
+    nove = {
+      id_ref: idaux,
+      asunto: 'Nuevo Curso',
+      detalle: 'Lanzamiento de curso:' + nombre,
+      fecha: (new Date(Date.now())).toLocaleDateString(),
+
+
+    }
+    await pool.query('insert novedades  set ?', [nove])
+    res.json('Realizado')
+  } catch (error) {
+    console.log(error)
+    res.json('Error algo sucedio')
+  }
+
+})
 
 
 
+router.post("/inscribir", isLoggedInn, async (req, res) => {
+  const { id, usuario } = req.body
+
+  try {
+    user = await pool.query('select * from usuarios where usuario =? ', [usuario])
+    nove = {
+      id_usuario: user[0]['id'],
+      id_curso: id,
+      inscripcion: 'Pendiente',
+      fecha: (new Date(Date.now())).toLocaleDateString(),
 
 
+    }
+    await pool.query('insert cursado  set ?', [nove])
+    res.json('Solicitud realizada ')
+
+  } catch (error) {
+    console.log(error)
+    res.json('Error algo sucedio')
+  }
+
+
+})
 
 
 
