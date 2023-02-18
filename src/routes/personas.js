a87aa538c187d3345a88c470e51504d8997e1f7c
Pipo <pipao.pipo@gmail.com>
2023-02-18 13:28:46 -0300

    xls

--- src/routes/personas.js
+++ src/routes/personas.js
@@ -2,17 +2,17 @@ const express = require('express')
 const router = express.Router()
 const { isLoggedIn, isLoggedInn, isLoggedInn2 } = require('../lib/auth') //proteger profile
 const pool = require('../database')
-
+const XLSX = require('xlsx')
 
 
 
 
 router.get('/datosusuario/:usuario', async (req, res) => {
   const usuario = req.params.usuario
- 
-  
-  
-  const aux =  await pool.query('select * from usuarios where usuario =?', [usuario])
+
+
+
+  const aux = await pool.query('select * from usuarios where usuario =?', [usuario])
 
   const etc = await pool.query('select * from personas where id =?', [aux[0]['id_persona']])
 
@@ -21,27 +21,27 @@ router.get('/datosusuario/:usuario', async (req, res) => {
 })
 router.get('/datospersona/:id', async (req, res) => {
   const id = req.params.id
- 
+
   try {
-  
-  
-  const aux =  await pool.query('select * from personas where id =?', [id])
 
-  
 
-  res.json([aux]);
-    
-} catch (error) {
-  console.log(error)
-  res.json(['']);
-}
+    const aux = await pool.query('select * from personas where id =?', [id])
+
+
+
+    res.json([aux]);
+
+  } catch (error) {
+    console.log(error)
+    res.json(['']);
+  }
   //res.render('index')
 })
 router.get('/lista', async (req, res) => {
   const usuario = req.params.usuario
- 
+
   const etc = await pool.query('select * from personas')
- 
+
   res.json(etc);
   //res.render('index')
 })
@@ -52,7 +52,7 @@ router.get('/datosusuarioporid/:id', isLoggedInn2, async (req, res) => {
   const aux = await pool.query('select * from usuarios where id =?', [id])
 
   const etc = await pool.query('select * from personas where id =?', [aux[0]['id_persona']])
- 
+
   porcentaje = 100
   console.log(etc[0]['trabajo'])
   if (etc[0]['trabajo'] == 'Si') {
@@ -82,11 +82,11 @@ router.get('/datosusuarioporid/:id', isLoggedInn2, async (req, res) => {
 
 router.post("/inscribir", isLoggedInn2, async (req, res) => {
   const { id_curso, id_usuario, accion } = req.body
-console.log(id_usuario+'   '+accion)
+  console.log(id_usuario + '   ' + accion)
 
-const aux = await pool.query('select * from usuarios where id =?', [id_usuario])
+  const aux = await pool.query('select * from usuarios where id =?', [id_usuario])
 
-const etc = await pool.query('select * from personas where id =?', [aux[0]['id_persona']])
+  const etc = await pool.query('select * from personas where id =?', [aux[0]['id_persona']])
 
 
   try {
@@ -95,19 +95,21 @@ const etc = await pool.query('select * from personas where id =?', [aux[0]['id_p
     }
 
 
-    if (accion == 'Aceptar') { 
+    if (accion == 'Aceptar') {
       console.log('opcion aceptar ')
-    act = {
-      inscripcion: 'Cursando'
-    }}
-   
-    if (accion == 'Rechazar') { 
-    act = {
-      inscripcion: 'Rechazado'
-    }}
+      act = {
+        inscripcion: 'Cursando'
+      }
+    }
+
+    if (accion == 'Rechazar') {
+      act = {
+        inscripcion: 'Rechazado'
+      }
+    }
 
 
-    await pool.query('UPDATE cursado set ? WHERE id_curso = ? and id_persona = ?  ', [act, id_curso,etc[0]['id']])
+    await pool.query('UPDATE cursado set ? WHERE id_curso = ? and id_persona = ?  ', [act, id_curso, etc[0]['id']])
     res.json('Realizado con exito ')
 
   } catch (error) {
@@ -121,37 +123,271 @@ const etc = await pool.query('select * from personas where id =?', [aux[0]['id_p
 
 router.post("/modificardatosadic", isLoggedInn, async (req, res) => {
   const { anios, trabajo, hijos, usuario } = req.body
-try {
-  const aux = await pool.query('select * from usuarios where usuario =?', [usuario])
+  try {
+    const aux = await pool.query('select * from usuarios where usuario =?', [usuario])
 
-const etc = await pool.query('select * from personas where id =?', [aux[0]['id_persona']])
+    const etc = await pool.query('select * from personas where id =?', [aux[0]['id_persona']])
 
-act = { anios, trabajo, hijos }  
+    act = { anios, trabajo, hijos }
 
-await pool.query('UPDATE personas set ?  where id = ?  ', [act, etc[0]['id']])
-res.json('Guardado con exito')
-} catch (error) {
-  console.log(error)
-  res.json('Error algo sucedio')
-}
+    await pool.query('UPDATE personas set ?  where id = ?  ', [act, etc[0]['id']])
+    res.json('Guardado con exito')
+  } catch (error) {
+    console.log(error)
+    res.json('Error algo sucedio')
+  }
 })
 
 
 
 
 router.post("/crear", isLoggedInn, async (req, res) => {
-  const { nombre, apellido, fecha_nac, trabajo, hijos,dni } = req.body
-try {
- 
-  etc= { nombre, apellido, fecha_nac, trabajo, hijos,dni }
-  console.log(etc)
-  await pool.query('insert personas  set ?', [etc])
-  console.log(1)
-res.json('Guardado con exito')
-} catch (error) {
-  console.log(error)
-  res.json('Error algo sucedio')
-}
+  const { nombre, apellido, fecha_nac, trabajo, hijos, dni } = req.body
+  try {
+
+    etc = { nombre, apellido, fecha_nac, trabajo, hijos, dni }
+    console.log(etc)
+    await pool.query('insert personas  set ?', [etc])
+    console.log(1)
+    res.json('Guardado con exito')
+  } catch (error) {
+    console.log(error)
+    res.json('Error algo sucedio')
+  }
 })
 
+
+
+
+
+
+
+
+
+
+
+
+///////// CARGAR PERSONAS DEL EXCEL
+router.get('/cargarpersonas', async (req, res) => {
+
+  const workbook = XLSX.readFile('./src/cargadepersonas/Muestreo.xlsx')
+  const workbooksheets = workbook.SheetNames
+  const sheet = workbooksheets[0]
+
+  const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
+  //console.log(dataExcel)
+
+
+  let a = 1
+  for (const property in dataExcel) {
+    a += 1
+    aux = dataExcel[property]['Número']
+    existe = await pool.query('select * from personas where dni = ?', [aux])
+
+
+    if (existe.length > 0) {
+      console.log('Dni ya existe')
+    } else {
+      hijos = 0
+      if (dataExcel[property]['En caso de haber respondido Si a la pregunta anterior, ¿Cuántos hijos tiene?'] === '') {
+        hijos = 0
+      } else {
+        hijos = dataExcel[property]['En caso de haber respondido Si a la pregunta anterior, ¿Cuántos hijos tiene?']
+      }
+
+      try {
+        const newLink = {
+          apellido: dataExcel[property]['Apellido'],
+          nombre: dataExcel[property]['Nombre'],
+          dni: dataExcel[property]['D.N.I.'],
+
+          usuario: 'No',
+          direccion: dataExcel[property]['Dirección calle'] + '-' + dataExcel[property][' Altura'] + '-' + dataExcel[property]['Piso y departamento (en caso que corresponda)'],
+          barrio: dataExcel[property]['Barrio'],
+          residencia: dataExcel[property]['Donde vivís'],
+          tel: dataExcel[property]['Número de teléfono de contacto'],
+
+          tel2: dataExcel[property]['tel2'],
+
+          participante_anterior: dataExcel[property]['¿Participaste de algún curso de la escuela de Mujeres? '],
+          nivel_secundario: dataExcel[property]['Nivel educativo alcanzado'],
+
+          hijos: hijos,
+          como_se_entero: dataExcel[property]['¿Cómo te enteraste de los cursos?'],
+
+
+        }
+
+
+
+        await pool.query('INSERT INTO personas set ?', [newLink]);
+
+
+        console.log('cargado')
+
+
+
+      } catch (e) {
+        console.log(e)
+      }
+
+      /* if ((dataExcel[property]['Sucursal']).includes(cuil_cuit)) {
+          estado = 'A'
+      }*/
+
+    }
+  }
+  console.log('finalizado')
+})
+
+
+
+
+router.get('/cargarcursos', async (req, res) => {
+
+  const workbook = XLSX.readFile('./src/cargadepersonas/Muestreo.xlsx')
+  const workbooksheets = workbook.SheetNames
+  const sheet = workbooksheets[0]
+
+  const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
+  //console.log(dataExcel)
+
+
+  let a = 1
+  for (const property in dataExcel) {
+    a += 1
+    aux = dataExcel[property]['Selecciona el primer curso de mayor preferencia (1)']
+    existe = await pool.query('select * from cursos where nombre = ?', [aux])
+
+
+    if (existe.length > 0) {
+      console.log('Dni ya existe')
+    } else {
+
+      aux = dataExcel[property]['Selecciona el primer curso de mayor preferencia (1)']
+      existe = await pool.query('select * from cursos where nombre = ?', [aux])
+      if (existe.length > 0) {
+        console.log('Curso ya existe')
+      } else {
+
+        aux = dataExcel[property]['Selecciona el primer curso de mayor preferencia (2)']
+        existe = await pool.query('select * from cursos where nombre = ?', [aux])
+
+        if (existe.length > 0) {
+          console.log('Curso ya existe')
+        } else {
+
+          aux = dataExcel[property]['Selecciona el primer curso de mayor preferencia (3)']
+          existe = await pool.query('select * from cursos where nombre = ?', [aux])
+
+          if (existe.length > 0) {
+            console.log('Curso ya existe')
+          } else {
+            try {
+              const newLink = {
+               
+                nombre: aux,
+              
+
+              }
+
+
+
+              await pool.query('INSERT INTO cursos set ?', [newLink]);
+
+
+              console.log('cargado')
+
+
+
+            } catch (e) {
+              console.log(e)
+            }
+          }
+        }
+
+
+      }
+    }
+  }
+  console.log('finalizado')
+})
+
+
+
+///////////
+
+
+///////// CARGAR INSCRIPCIONES
+router.get('/cargarpersonas', async (req, res) => {
+
+  const workbook = XLSX.readFile('./src/cargadepersonas/Muestreo.xlsx')
+  const workbooksheets = workbook.SheetNames
+  const sheet = workbooksheets[0]
+
+  const dataExcel = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
+  //console.log(dataExcel)
+
+
+  let a = 1
+  for (const property in dataExcel) {
+    a += 1
+    aux = dataExcel[property]['Número']
+    existe = await pool.query('select * from personas where dni = ?', [aux])
+
+
+    if (existe.length > 0) {
+      console.log('Dni ya existe')
+    } else {
+      hijos = 0
+      if (dataExcel[property]['En caso de haber respondido Si a la pregunta anterior, ¿Cuántos hijos tiene?'] === '') {
+        hijos = 0
+      } else {
+        hijos = dataExcel[property]['En caso de haber respondido Si a la pregunta anterior, ¿Cuántos hijos tiene?']
+      }
+
+      try {
+        const newLink = {
+          apellido: dataExcel[property]['Apellido'],
+          nombre: dataExcel[property]['Nombre'],
+          dni: dataExcel[property]['D.N.I.'],
+
+          usuario: 'No',
+          direccion: dataExcel[property]['Dirección calle'] + '-' + dataExcel[property][' Altura'] + '-' + dataExcel[property]['Piso y departamento (en caso que corresponda)'],
+          barrio: dataExcel[property]['Barrio'],
+          residencia: dataExcel[property]['Donde vivís'],
+          tel: dataExcel[property]['Número de teléfono de contacto'],
+
+          tel2: dataExcel[property]['tel2'],
+
+          participante_anterior: dataExcel[property]['¿Participaste de algún curso de la escuela de Mujeres? '],
+          nivel_secundario: dataExcel[property]['Nivel educativo alcanzado'],
+
+          hijos: hijos,
+          como_se_entero: dataExcel[property]['¿Cómo te enteraste de los cursos?'],
+
+
+        }
+
+
+
+        await pool.query('INSERT INTO personas set ?', [newLink]);
+
+
+        console.log('cargado')
+
+
+
+      } catch (e) {
+        console.log(e)
+      }
+
+      /* if ((dataExcel[property]['Sucursal']).includes(cuil_cuit)) {
+          estado = 'A'
+      }*/
+
+    }
+  }
+  console.log('finalizado')
+})
 module.exports = router
