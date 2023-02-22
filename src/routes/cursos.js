e416bf79cd25e694cda920663ab1f4f382727ec0
Pipo <pipao.pipo@gmail.com>
2023-02-22 09:40:55 -0300

    detalle curso

--- src/routes/cursos.js
+++ src/routes/cursos.js
@@ -22,18 +22,19 @@ router.get('/listaniv1/:usuario', isLoggedInn, async (req, res) => {
 
   const aux = await pool.query('select * from usuarios where usuario =?', [usuario])
 
-const etc2 = await pool.query('select * from personas where id =?', [aux[0]['id_persona']])
- 
+  const etc2 = await pool.query('select * from personas where id =?', [aux[0]['id_persona']])
 
-console.log(etc2[0]['id'])
-//console.log(etc)
 
-const etc3 = await pool.query('select cursos.id,cursos.fecha , encargado, nombre, cupo, cursos.id, c.inscripcion, c.id_persona from cursos left join (select * from cursado where id_persona = ? ) c on cursos.id=c.id_curso  ', [etc2[0]['id']])
+  console.log(etc2[0]['id'])
+  //console.log(etc)
 
-try {
-///const etc4 = await pool.query('select * from cursos join  cursado ')
+  const etc3 = await pool.query('select cursos.id,cursos.fecha , encargado, nombre, cupo, cursos.id, c.inscripcion, c.id_persona from cursos left join (select * from cursado where id_persona = ? ) c on cursos.id=c.id_curso  ', [etc2[0]['id']])
 
-  res.json(etc3);} catch (error) {
+  try {
+    ///const etc4 = await pool.query('select * from cursos join  cursado ')
+
+    res.json(etc3);
+  } catch (error) {
     console.log(Error)
     res.json('Error algo salio mal')
   }
@@ -68,16 +69,16 @@ router.get('/verclases/:id', isLoggedInn2, async (req, res) => {
 router.get('/asistencia/:id', isLoggedInn2, async (req, res) => {
   const id = req.params.id
 
-try {
-   const clase  = await pool.query('select * from clases where id = ?',[id])
-   
-   const alumnos = await pool.query('select * from cursado left  join asistencia on cursado.id = asistencia.id_cursado   join usuarios on  cursado.id_usuario = usuarios.id    where cursado.id_curso = ?  and cursado.inscripcion= "Cursando" ',[clase[0]['id_curso']])
-   
-   console.log(alumnos)
-res.json([clase,alumnos])
-} catch (error) {
-  console.log(error)
-}
+  try {
+    const clase = await pool.query('select * from clases where id = ?', [id])
+
+    const alumnos = await pool.query('select * from cursado left  join asistencia on cursado.id = asistencia.id_cursado   join usuarios on  cursado.id_usuario = usuarios.id    where cursado.id_curso = ?  and cursado.inscripcion= "Cursando" ', [clase[0]['id_curso']])
+
+    console.log(alumnos)
+    res.json([clase, alumnos])
+  } catch (error) {
+    console.log(error)
+  }
 
 
 })
@@ -88,95 +89,193 @@ res.json([clase,alumnos])
 router.get('/detalledelcurso/:id', isLoggedInn2, async (req, res) => {
   const id = req.params.id
   try {
-      /////CLASES DEL CURSO
+    /////CLASES DEL CURSO
     const etc = await pool.query('select * from clases where id_curso = ? ', [id])
-   //////pendientes inscriptos prioridad 1 2 3
-    const pendientes1 = await pool.query('select * from inscripciones join personas on inscripciones.dni_persona = personas.dni where uno=? and estado ="pendiente" ',[id])
+    //////pendientes inscriptos prioridad 1 2 3
+    const pendientes1 = await pool.query('select * from inscripciones join personas on inscripciones.dni_persona = personas.dni where uno=? and estado ="pendiente" ', [id])
 
-    const pendientes2 = await pool.query('select * from inscripciones join personas on inscripciones.dni_persona = personas.dni where dos=? and estado ="pendiente" ',[id])
-    const pendientes3 = await pool.query('select * from inscripciones join personas on inscripciones.dni_persona = personas.dni where dos=? and estado ="pendiente" ',[id])
-    
-   cursado = await pool.query('select * from cursado where id_curso = ?',[id])
+    const pendientes2 = await pool.query('select * from inscripciones join personas on inscripciones.dni_persona = personas.dni where dos=? and estado ="pendiente" ', [id])
+    const pendientes3 = await pool.query('select * from inscripciones join personas on inscripciones.dni_persona = personas.dni where dos=? and estado ="pendiente" ', [id])
+
+    cursado = await pool.query('select * from cursado where id_curso = ?', [id])
 
 
-    array1=pendientes1.concat(pendientes2);
+    array1 = pendientes1.concat(pendientes2);
 
-    array1= array1.concat(pendientes3);
-console.log(array1.length)
+    array1 = array1.concat(pendientes3);
+    console.log(array1.length)
     /////isncripciones si participo/no participo
     //si
-    const cursadosi = await pool.query('select * from inscripciones join personas on inscripciones.dni_persona =personas.dni  where inscripciones.uno=? and personas.participante_anterior="Sí"',[id])
+    const cursadosi = await pool.query('select * from inscripciones join personas on inscripciones.dni_persona =personas.dni  where inscripciones.uno=? and personas.participante_anterior="Sí"', [id])
     //no
-    const cursadono = await pool.query('select * from inscripciones join personas on inscripciones.dni_persona =personas.dni  where inscripciones.uno=? and personas.participante_anterior="No"',[id])
-///////datos del curso
+    const cursadono = await pool.query('select * from inscripciones join personas on inscripciones.dni_persona =personas.dni  where inscripciones.uno=? and personas.participante_anterior="No"', [id])
+    ///////datos del curso
     const curso = await pool.query('select * from cursos where id = ? ', [id])
     ////////////ALUMNOS YAINSCRIPTOS
-    const inscriptos = await pool.query('select * from cursado where id_curso=? and inscripcion ="Cursando"',[id])
+    const inscriptos = await pool.query('select * from cursado where id_curso=? and inscripcion ="Cursando"', [id])
     ///CLASES DEL CURSO
-    const clases = await pool.query('select * from clases where id_curso=? ',[id])
+    const clases = await pool.query('select * from clases where id_curso=? ', [id])
 
-    //////estadisticas
- lista=[]
- auxil = {dato:"Cupo",
- cantidad:curso[0]['cupo']
-}
- lista.push(auxil)
+    ///---------------------------------------------------
+    //////ARMADO DE TABLA DL CURSO
 
-auxil = {dato:"Participo/tiene hijos/trabaja formal",
-cantidad:(curso[0]['cupo']*0.012285).toFixed(2)
-}
-lista.push(auxil)
-auxil = {dato:"Participo/tiene hijos/trabaja Informal",
-cantidad:(curso[0]['cupo']*0.022815.toFixed(2))
-}
-lista.push(auxil)
-auxil = {dato:"Participo/tiene hijos/No trabaja",
-cantidad:(curso[0]['cupo']*0.3159).toFixed(2)
-}
- lista.push(auxil)
-
-auxil = {dato:"Participo/No tiene hijos/No trabaja",
-cantidad:curso[0]['cupo']*0.08415.toFixed(2)
-}
-lista.push(auxil)
-
-auxil = {dato:"Participo/tiene hijos/trabaja",
-cantidad:(curso[0]['cupo']*0.01485).toFixed(2)
-}
- lista.push(auxil)
-
-auxil = {dato:"No Participo/tiene hijos/trabaja Formalmente ",
-cantidad:(curso[0]['cupo']*0.0561).toFixed(2)
-}
- lista.push(auxil)
-
-auxil = {dato:"No Participo/tiene hijos/trabaja Inormalmente ",
-cantidad:(curso[0]['cupo']*0.1309).toFixed(2)
-}
- lista.push(auxil)
-
-auxil = {dato:"No Participo/tiene hijos/No trabaja ",
-cantidad:(curso[0]['cupo']*0.187).toFixed(2)
-}
- lista.push(auxil)
+    cursado = await pool.query('select * from cursado where id_curso = ? ', [id])
+    unounouno = 0
+    unounodosuno = 0
+    unounodosdos = 0
+    unodosuno = 0
+    unodosdosuno = 0
+    unodosdosdos = 0
+    dosunouno = 0
+    dosunodosuno = 0
+    dosunodosdos = 0
+    dosdosuno = 0
+    dosdosdos = 0
 
 
-auxil = {dato:"No Participo/No tiene hijos/No trabaja ",
-cantidad:(curso[0]['cupo']*0.1232).toFixed(2)
-}
- lista.push(auxil)
+    for (ii in cursado) {
 
-auxil = {dato:"No Participo/No tiene hijos/Trabaja Formalmente ",
-cantidad:(curso[0]['cupo']*0.0262).toFixed(2)
-}
- lista.push(auxil)
+      switch (cursado[ii]['categoria']) {
+        case "1.1.1":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          unounouno += 1
+          break;
+        case "1.1.2.1":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          unounodosuno += 1
+          break
+        case "1.1.2.2":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          unounodosdos += 1
+          break;
+        case "1.2.1":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          unodosuno += 1
+          break;
+        case "1.2.2.1":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          unodosdosuno += 1
+          break;
+        case "1.2.2.2":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          unodosdosdos += 1
+          break;
+        case "2.1.1":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          dosunouno += 1
+          break;
+        case "1.2.2.2":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          unodosdosdos += 1
+          break;
+        case "2.1.1":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          dosunouno += 1
+          break;
+        case "2.1.2.1":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          dosunodosuno += 1
+          break;
+        case "2.1.2.2":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          dosunodosdos += 1
+          break;
+        case "2.2.1":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          dosdosuno += 1
+          break;
+        case "2.2.2":
+          //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+          dosdosdos += 1
+          break;
 
-auxil = {dato:"No Participo/No tiene hijos/Trabaja Informalmente ",
-cantidad:(curso[0]['cupo']*0.0262).toFixed(2)
-}
- lista.push(auxil)
+      }
+    }
 
-    res.json([etc,array1,[cursadosi,cursadono],curso,inscriptos.length,clases,lista,cursado]);
+    lista = []
+    auxil = {
+      dato: "Cupo",
+      cantidad: curso[0]['cupo']
+    }
+    lista.push(auxil)
+
+    auxil = {
+      dato: "Participo/tiene hijos/trabaja formal",
+      cantidad: (curso[0]['cupo'] * 0.012285).toFixed(2),
+      aceptados:dosunodosdos
+    }
+    lista.push(auxil)
+    auxil = {
+      dato: "Participo/tiene hijos/trabaja Informal",
+      cantidad: (curso[0]['cupo'] * 0.022815.toFixed(2)),
+      aceptados:dosunodosuno
+    }
+    lista.push(auxil)
+    auxil = {
+      dato: "Participo/tiene hijos/No trabaja",
+      cantidad: (curso[0]['cupo'] * 0.3159).toFixed(2),
+      aceptados:dosunouno
+    }
+    lista.push(auxil)
+
+    auxil = {
+      dato: "Participo/No tiene hijos/No trabaja",
+      cantidad: curso[0]['cupo'] * 0.08415.toFixed(2),
+      aceptados:dosdosuno
+    }
+    lista.push(auxil)
+
+    auxil = {
+      dato: "Participo/tiene hijos/trabaja",
+      cantidad: (curso[0]['cupo'] * 0.01485).toFixed(2),
+      aceptados:dosdosdos
+    }
+    lista.push(auxil)
+
+    auxil = {
+      dato: "No Participo/tiene hijos/trabaja Formalmente ",
+      cantidad: (curso[0]['cupo'] * 0.0561).toFixed(2),
+      aceptados:unounodosdos
+    }
+    lista.push(auxil)
+
+    auxil = {
+      dato: "No Participo/tiene hijos/trabaja Inormalmente ",
+      cantidad: (curso[0]['cupo'] * 0.1309).toFixed(2),
+      aceptados:unounodosuno
+    }
+    lista.push(auxil)
+
+    auxil = {
+      dato: "No Participo/tiene hijos/No trabaja ",
+      cantidad: (curso[0]['cupo'] * 0.187).toFixed(2),
+      aceptados:unounouno
+    }
+    lista.push(auxil)
+
+
+    auxil = {
+      dato: "No Participo/No tiene hijos/No trabaja ",
+      cantidad: (curso[0]['cupo'] * 0.1232).toFixed(2),
+      aceptados:unodosuno
+    }
+    lista.push(auxil)
+
+    auxil = {
+      dato: "No Participo/No tiene hijos/Trabaja Formalmente ",
+      cantidad: (curso[0]['cupo'] * 0.0262).toFixed(2),
+      aceptados:unounodosdos
+    }
+    lista.push(auxil)
+
+    auxil = {
+      dato: "No Participo/No tiene hijos/Trabaja Informalmente ",
+      cantidad: (curso[0]['cupo'] * 0.0262).toFixed(2),
+      aceptados:dosunodosuno
+    }
+    lista.push(auxil)
+
+    res.json([etc, array1, [cursadosi, cursadono], curso, inscriptos.length, clases, lista, cursado]);
   } catch (error) {
     console.log(error)
     res.json(['']);
@@ -227,20 +326,20 @@ router.post("/crear", isLoggedInn2, async (req, res) => {
 
 router.post("/nuevaclase", isLoggedInn2, async (req, res) => {
   const { id_curso, fecha, observaciones } = req.body
-console.log(id_curso)
-console.log(fecha)
-try {
-  
+  console.log(id_curso)
+  console.log(fecha)
+  try {
 
-const nuev = {
-  id_curso, fecha ,observacion:observaciones
-}
-await pool.query('insert clases  set ?', [nuev])
-res.json('Cargada nueva clase')
-} catch (error) {
-  console.log(error)
-  res.json('Error algo sucedio')
-}
+
+    const nuev = {
+      id_curso, fecha, observacion: observaciones
+    }
+    await pool.query('insert clases  set ?', [nuev])
+    res.json('Cargada nueva clase')
+  } catch (error) {
+    console.log(error)
+    res.json('Error algo sucedio')
+  }
 
 })
 
@@ -254,7 +353,7 @@ router.post("/inscribir", isLoggedInn, async (req, res) => {
 
 
     nove = {
-      id_persona:etc,
+      id_persona: etc,
       id_curso: id,
       inscripcion: 'Pendiente',
       fecha: (new Date(Date.now())).toLocaleDateString(),
@@ -274,7 +373,7 @@ router.post("/inscribir", isLoggedInn, async (req, res) => {
 
 
 router.post("/presente", isLoggedInn2, async (req, res) => {
-  const { id_usuario, id} = req.body ///
+  const { id_usuario, id } = req.body ///
   console.log(id_usuario)
 })
 
