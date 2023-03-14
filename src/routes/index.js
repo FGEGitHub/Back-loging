8251e70d976a5aabe3649095a6bd355b1928bbd7
Pipo <pipao.pipo@gmail.com>
2023-03-14 12:30:27 -0300

    index

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,10 +10,10 @@ const pool = require('../database')
 
 router.get('/', async (req, res) => {
     console.log('hola')
-    const etc = await pool.query ('select * from usuarios' )
+    
   
 
-  res.json(etc);
+  res.json('hola');
 //res.render('index')
 })
 
