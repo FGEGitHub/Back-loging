75cd179ab4dac9c2941b190491a9c70de608b64f
Pipo <pipao.pipo@gmail.com>
2023-03-15 10:29:24 -0300

    keys

--- src/keys.js
+++ src/keys.js
@@ -1,16 +1,12 @@
-const DB_HOST = require('./config')
-const DB_NAME = require('./config')
-const DB_PASSWORD = require('./config')
-const DB_USER = require('./config')
-const DB_PORT = require('./config')
 
 module.exports = {
     database:{
-        host: process.env.DB_HOST   || 'localhost',
-        user: process.env.DB_USER   || 'admin',
+        host: 'localhost',
+       // user: 'root',
+            user: process.env.DB_USER   || 'admin',
+      //  password: 'root',
         password: process.env.DB_PASSWORD   || '11235',
-        database: process.env.DB_NAME    || 'marketing',
-        DB_PORT:  process.env.DB_PORT   || '3306',
+        database: 'marketing'
     }
 
 }
