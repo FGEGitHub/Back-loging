75661825d2c7bddb078046ff8ad82d2c3aa57da9
Pipo <pipao.pipo@gmail.com>
2023-03-15 10:07:12 -0300

    index cors

--- src/index.js
+++ src/index.js
@@ -45,7 +45,12 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(cors());
 
-
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 //globalvariables
 /* app.use((req,res,next)=>{
