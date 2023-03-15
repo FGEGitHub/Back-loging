80930b9919592ebe6ae2d3a10e7fe2c89e448855
Pipo <pipao.pipo@gmail.com>
2023-03-15 09:46:21 -0300

    pass bd

--- src/keys.js
+++ src/keys.js
@@ -7,8 +7,8 @@ const DB_PORT = require('./config')
 module.exports = {
     database:{
         host: process.env.DB_HOST   || 'localhost',
-        user: process.env.DB_USER   || 'root',
-        password: process.env.DB_PASSWORD   || 'root',
+        user: process.env.DB_USER   || 'admin',
+        password: process.env.DB_PASSWORD   || '11235',
         database: process.env.DB_NAME    || 'marketing',
         DB_PORT:  process.env.DB_PORT   || '3306',
     }
