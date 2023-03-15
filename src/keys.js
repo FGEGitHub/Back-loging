2038d448af87e2d15ea0b535efd14e38a0e51ea5
Pipo <pipao.pipo@gmail.com>
2023-03-15 10:30:35 -0300

    keys2

--- src/keys.js
+++ src/keys.js
@@ -3,9 +3,9 @@ module.exports = {
     database:{
         host: 'localhost',
        // user: 'root',
-            user: process.env.DB_USER   || 'admin',
-      //  password: 'root',
-        password: process.env.DB_PASSWORD   || '11235',
+            user: 'admin',
+       // password: 'root',
+        password:'11235',
         database: 'marketing'
     }
 
