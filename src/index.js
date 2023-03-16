8c6f087c0014a9ed426bab6f1d1afc7f91679455
Pipo <pipao.pipo@gmail.com>
2023-03-16 18:07:52 -0300

    sesion

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 const jwt = require('jsonwebtoken')
 const keys = require('./keys')
 ///
-//const pool = require('./database')
+const pool = require('./database')
 
 
 ////
@@ -31,12 +31,13 @@ app.set('view engine', '.hbs')
 
 
 //middlwares
+/*
 app.use(session({
     secret: 'faztmysqlnodesession',
     resave: false,
     saveUninitialized: false,
     store: new MySQLStore(database)
-}))
+}))*/
 
 app.use(flash())
 app.use(morgan('dev'))
@@ -46,12 +47,11 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(cors());
 
-const corsOptions ={
+/* const corsOptions ={
     origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+} */
 
 //globalvariables
 /* app.use((req,res,next)=>{
