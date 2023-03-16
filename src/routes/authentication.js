9ea849388ba9901e307fd8b66f05ea67cdefc2d8
Pipo <pipao.pipo@gmail.com>
2023-03-16 19:49:02 -0300

    router all

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -9,7 +9,13 @@ const pool = require('../database')
 const jwt = require('jsonwebtoken')
 
 
-
+router.all('*', function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept")
+    res.header("Access-Control-Max-Age", "1728000")
+    next();
+});
 
 
 
