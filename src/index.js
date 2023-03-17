fecce9fe645c1f7601767541d5977f7036b11e14
Pipo <pipao.pipo@gmail.com>
2023-03-17 00:10:24 -0300

    cors options

--- src/index.js
+++ src/index.js
@@ -76,12 +76,12 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(cors());
 
-/* const corsOptions ={
-    origin:'http://localhost:3000', 
+ const corsOptions ={
+    origin:'http://esme.cuquicalvano.com:3000/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
-} */
-
+} 
+app.use(cors(corsOptions));
 //globalvariables
 /* app.use((req,res,next)=>{
     app.locals.success = req.flash('success')
