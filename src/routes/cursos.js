807d11efc50214b456576db4322c0b804282e399
Pipo <pipao.pipo@gmail.com>
2023-03-22 19:56:06 -0300

    cursos

--- src/routes/cursos.js
+++ src/routes/cursos.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { isLoggedIn, isLoggedInn, isLoggedInn2,isLoggedInn4 } = require('../lib/auth') //proteger profile
+const { isLoggedIn, isLoggedInn, isLoggedInn2, isLoggedInn4 } = require('../lib/auth') //proteger profile
 const pool = require('../database')
 
 
@@ -15,49 +15,49 @@ router.get('/lista/', isLoggedInn4, async (req, res) => {
 })
 
 router.get('/datosdelturno/:id', isLoggedInn4, async (req, res) => {
-try {
-  const id = req.params.id
-  
-  turno = await pool.query('select nombrecurso, turnos.id,  id_encargado, id_coordinador from turnos join (Select id as idcurso, nombre as nombrecurso from cursos) as selec1 on turnos.id_curso=selec1.idcurso where turnos.id=?  ',[id])
-  cantidad = await pool.query ('select * from cursado where id_turno  =?',[id])
-  cant={
-    cantidad:cantidad.length
-  }
-  array1 = turno.concat(cant);
- 
-  encarg= await pool.query('select * from usuarios where id = ?',[turno[0]['id_encargado']])
   try {
-    en={
-      encargado:encarg[0]['nombre']
+    const id = req.params.id
+
+    turno = await pool.query('select nombrecurso, turnos.id,  id_encargado, id_coordinador from turnos join (Select id as idcurso, nombre as nombrecurso from cursos) as selec1 on turnos.id_curso=selec1.idcurso where turnos.id=?  ', [id])
+    cantidad = await pool.query('select * from cursado where id_turno  =?', [id])
+    cant = {
+      cantidad: cantidad.length
     }
+    array1 = turno.concat(cant);
+
+    encarg = await pool.query('select * from usuarios where id = ?', [turno[0]['id_encargado']])
+    try {
+      en = {
+        encargado: encarg[0]['nombre']
+      }
+    } catch (error) {
+      en = {
+        encargado: 'Sin definir'
+      }
+    }
+
+    array2 = array1.concat(en);
+
+    coor = await pool.query('select * from usuarios where id = ?', [turno[0]['id_coordinador']])
+    try {
+      co = {
+        coordinador: coor[0]['nombre']
+      }
+    } catch (error) {
+      co = {
+        coordinador: 'sin definir'
+      }
+    }
+
+    array3 = array2.concat(co);
+
+
+
+    res.json(array3)
   } catch (error) {
-    en={
-      encargado:'Sin definir'
-    }
+    console.log(error)
+    res.json(['nd', 'nd', 'nd', 'nd'])
   }
-
-  array2 = array1.concat(en);
-
-  coor= await pool.query('select * from usuarios where id = ?',[turno[0]['id_coordinador']])
-  try {
-    co={
-      coordinador:coor[0]['nombre']
-    }
-  } catch (error) {
-    co={
-      coordinador:'sin definir'
-    }
-  }
-
-  array3 = array2.concat(co);
-
-
-
-res.json(array3)
-} catch (error) {
-  console.log(error)
-  res.json(['nd','nd','nd','nd'])
-}
 
 
 
@@ -65,7 +65,7 @@ res.json(array3)
 
 
 
-  
+
 /////// lista desde el usuario 1
 router.get('/listaniv1/:usuario', isLoggedInn, async (req, res) => {
   const usuario = req.params.usuario
@@ -77,7 +77,7 @@ router.get('/listaniv1/:usuario', isLoggedInn, async (req, res) => {
 
 
   try {
-  const etc3 = await pool.query('select cursos.id,cursos.fecha , encargado, nombre, cupo, cursos.id, c.inscripcion, c.id_persona from cursos left join (select * from cursado where id_persona = ? ) c on cursos.id=c.id_curso  ', [etc2[0]['id']])
+    const etc3 = await pool.query('select cursos.id,cursos.fecha , encargado, nombre, cupo, cursos.id, c.inscripcion, c.id_persona from cursos left join (select * from cursado where id_persona = ? ) c on cursos.id=c.id_curso  ', [etc2[0]['id']])
 
 
     ///const etc4 = await pool.query('select * from cursos join  cursado ')
@@ -117,68 +117,68 @@ router.get('/verclases/:id', isLoggedInn2, async (req, res) => {
 
 router.get('/asistencia/:id', isLoggedInn4, async (req, res) => {
   const id = req.params.id
-console.log('asistencia')
+  console.log('asistencia')
   try {
     /// trae la clase
     const clase = await pool.query('select * from clases where id = ?', [id])
-//// trae el listado de alumnos  que cursan en ese turno
+    //// trae el listado de alumnos  que cursan en ese turno
 
     const alumnos = await pool.query('select *, id as idcursado from cursado join   (select nombre,apellido, id as idpersona from personas) as  personaa on cursado.id_persona=personaa.idpersona  where cursado.id_turno = ?  and cursado.inscripcion= "Confirmado" ', [clase[0]['id_turno']])
 
     total = alumnos.length
-    presentes=0
-    ausentes=0
+    presentes = 0
+    ausentes = 0
     notomados = 0
     ////recorremos por la asistencia
-    asistenciaa=[]
+    asistenciaa = []
     for (ii in alumnos) {
 
-        asis = await pool.query('select * from asistencia where id_persona = ? and id_clase = ?',[alumnos[ii]['id_persona'],id])
-        console.log(asis)
+      asis = await pool.query('select * from asistencia where id_persona = ? and id_clase = ?', [alumnos[ii]['id_persona'], id])
+      console.log(asis)
 
       if (asis.length === 0) {
         notomados += 1
         aux = {
-          id_alumno:alumnos[ii]['id_persona'],
+          id_alumno: alumnos[ii]['id_persona'],
           nombre: alumnos[ii]['nombre'],
           apellido: alumnos[ii]['apellido'],
-          dni:alumnos[ii]['dni'],
-          asistencia:'No Tomada',
-          id_clase:id
-         
+          dni: alumnos[ii]['dni'],
+          asistencia: 'No Tomada',
+          id_clase: id
+
         }
         asistenciaa.push(aux)
-        }else{
-          if(asis[0]['asistencia'] ==='Presente'){
-            presentes +=1
-          }else{
-            ausentes+=1
-          }
-    
-          aux = {
-            id_alumno:alumnos[ii]['id_persona'],
-            nombre: alumnos[ii]['nombre'],
-            apellido: alumnos[ii]['apellido'],
-            dni:alumnos[ii]['dni'],
-            asistencia:asis[0]['asistencia'],
-            id_clase:id
-           
-          }
-          asistenciaa.push(aux)
+      } else {
+        if (asis[0]['asistencia'] === 'Presente') {
+          presentes += 1
+        } else {
+          ausentes += 1
         }
-      
+
+        aux = {
+          id_alumno: alumnos[ii]['id_persona'],
+          nombre: alumnos[ii]['nombre'],
+          apellido: alumnos[ii]['apellido'],
+          dni: alumnos[ii]['dni'],
+          asistencia: asis[0]['asistencia'],
+          id_clase: id
+
+        }
+        asistenciaa.push(aux)
+      }
+
 
     }
 
 
-   estadisticas = {
-    presentes,
-    ausentes,
-    notomados,
+    estadisticas = {
+      presentes,
+      ausentes,
+      notomados,
 
-   }
+    }
 
-    res.json([clase, asistenciaa,estadisticas])
+    res.json([clase, asistenciaa, estadisticas])
   } catch (error) {
     console.log(error)
   }
@@ -193,24 +193,24 @@ router.get('/borrarturno/:id', async (req, res) => {
   console.log(id)
   try {
 
-try {
+    try {
 
-  cursado = await pool.query('select * from cursado where id_turno = ?',[id])
+      cursado = await pool.query('select * from cursado where id_turno = ?', [id])
 
-  for (ii in cursado) {
-
-
-
-  await pool.query('update incripciones set estado="pendiente" where id=?  ', [cursado[ii]['id_inscripcion']])
-}
-} catch (error) {
-  console.log(error)
-}
+      for (ii in cursado) {
 
 
-    await pool.query('delete  from  cursado where id_turno = ?',[id])
-    await pool.query('delete  from  inscripciones where id = ?',[id])
-    await pool.query('delete  from  turnos where id = ?',[id])
+
+        await pool.query('update incripciones set estado="pendiente" where id=?  ', [cursado[ii]['id_inscripcion']])
+      }
+    } catch (error) {
+      console.log(error)
+    }
+
+
+    await pool.query('delete  from  cursado where id_turno = ?', [id])
+    await pool.query('delete  from  inscripciones where id = ?', [id])
+    await pool.query('delete  from  turnos where id = ?', [id])
     console.log('realizado')
     res.json('realizado')
   } catch (error) {
@@ -224,51 +224,51 @@ try {
 
 
 router.get('/listadetodoslosturnos/', isLoggedInn2, async (req, res) => {
-try {
-  console.log('listadetodos')
-  turnos = await pool.query('select * from turnos   join  (select id as idcurso, nombre as nombrecurso from cursos) as selec1  on turnos.id_curso= selec1.idcurso ')
+  try {
+    console.log('listadetodos')
+    turnos = await pool.query('select * from turnos   join  (select id as idcurso, nombre as nombrecurso from cursos) as selec1  on turnos.id_curso= selec1.idcurso ')
 
 
-  todos = []
-  for (ii in turnos) {
-    cat = await pool.query('select * from cursado where id_turno= ?',[turnos[ii]['id']])
-    faltan = await pool.query('select * from cursado where id_turno= ? and (inscripcion <> "Confirmado" and inscripcion <> "Rechazado") ',[turnos[ii]['id']])
-   en =  await pool.query('select * from usuarios where id= ?',[turnos[ii]['id_encargado']])
-   co =  await pool.query('select * from usuarios where id= ?',[turnos[ii]['id_coordinador']])
-   enc='sin determinar'
-   if(en.length>0) {
-    enc= en[0]['nombre']
-   }
+    todos = []
+    for (ii in turnos) {
+      cat = await pool.query('select * from cursado where id_turno= ?', [turnos[ii]['id']])
+      faltan = await pool.query('select * from cursado where id_turno= ? and (inscripcion <> "Confirmado" and inscripcion <> "Rechazado") ', [turnos[ii]['id']])
+      en = await pool.query('select * from usuarios where id= ?', [turnos[ii]['id_encargado']])
+      co = await pool.query('select * from usuarios where id= ?', [turnos[ii]['id_coordinador']])
+      enc = 'sin determinar'
+      if (en.length > 0) {
+        enc = en[0]['nombre']
+      }
 
-   coor='sin determinar'
-   if(en.length>0) {
-    coor= en[0]['nombre']
-   }
+      coor = 'sin determinar'
+      if (co.length > 0) {
+        coor = co[0]['nombre']
+      }
 
 
-   nuev = {
-      id: turnos[ii]['id'],
-      id_curso: turnos[ii]['id_curso'],
-      numero: turnos[ii]['numero'],
-      descripcion: turnos[ii]['descripcion'],
-     enc,
-   coor,
-      idcurso: 123,
-      nombrecurso: turnos[ii]['nombrecurso'],
-      id_turno: turnos[ii]['id_turno'],
-      cant: cat.length,
-      faltanporresp:faltan.length
+      nuev = {
+        id: turnos[ii]['id'],
+        id_curso: turnos[ii]['id_curso'],
+        numero: turnos[ii]['numero'],
+        descripcion: turnos[ii]['descripcion'],
+        enc,
+        coor,
+        idcurso: 123,
+        nombrecurso: turnos[ii]['nombrecurso'],
+        id_turno: turnos[ii]['id_turno'],
+        cant: cat.length,
+        faltanporresp: faltan.length
+      }
+      todos.push(nuev)
     }
-    todos.push(nuev)
+
+
+    res.json(todos)
+  } catch (error) {
+    console.log(error)
+    res.json([])
   }
 
-
-  res.json(todos)
-} catch (error) {
-console.log(error)
-  res.json([])
-}
- 
 
 })
 
@@ -276,31 +276,31 @@ console.log(error)
 
 router.get('/listadeturnos/:id', isLoggedInn2, async (req, res) => {
   const id = req.params.id
- 
+
   turnos = await pool.query('select * from turnos  where id_curso = ?', [id])
 
   todos = []
   for (ii in turnos) {
-  
-   // cursado = await pool.query('select *, turnos.id id, usuarios.nombre coordinador from cursado join turnos on cursado.id_turno=turnos.id join personas on cursado.id_persona =personas.id  join usuarios on turnos.id_coordinador = usuarios.id where cursado.id_curso = ? and cursado.id_turno =?', [id, turnos[ii]['id']])
-cursado=await pool.query('select *  from cursado  join(select id as idturno, descripcion, id_coordinador, id_encargado from turnos ) as selec1 on cursado.id_turno = selec1.idturno  left join (select id as idpersona, nombre as nombrepersona,apellido as apellidopersona, dni from personas) as selec2 on cursado.id_persona = selec2.idpersona left join (select id as idusuario, nombre as nombrecoordinador from usuarios ) as selec3 on  selec1.id_coordinador = selec3.idusuario left join (select id as  idusuarioo, nombre as nombreencargado from usuarios ) as selec4 on selec1.id_encargado = selec4.idusuarioo where cursado.id_turno =? ', [ turnos[ii]['id']])
+
+    // cursado = await pool.query('select *, turnos.id id, usuarios.nombre coordinador from cursado join turnos on cursado.id_turno=turnos.id join personas on cursado.id_persona =personas.id  join usuarios on turnos.id_coordinador = usuarios.id where cursado.id_curso = ? and cursado.id_turno =?', [id, turnos[ii]['id']])
+    cursado = await pool.query('select *  from cursado  join(select id as idturno, descripcion, id_coordinador, id_encargado from turnos ) as selec1 on cursado.id_turno = selec1.idturno  left join (select id as idpersona, nombre as nombrepersona,apellido as apellidopersona, dni from personas) as selec2 on cursado.id_persona = selec2.idpersona left join (select id as idusuario, nombre as nombrecoordinador from usuarios ) as selec3 on  selec1.id_coordinador = selec3.idusuario left join (select id as  idusuarioo, nombre as nombreencargado from usuarios ) as selec4 on selec1.id_encargado = selec4.idusuarioo where cursado.id_turno =? ', [turnos[ii]['id']])
 
     if (cursado.length > 0) {
       todos.push(cursado)
-      }else{
-     
-        cursado = {
-          id_turno:turnos[ii]['id'],
-          turno: turnos[ii]['numero'],
-          descripcion: turnos[ii]['descripcion'],
-          encargado:turnos[ii]['encargado'],
-          motivo:turnos[ii]['motivo'],
-         
-        }
-        todos.push([cursado])
+    } else {
+
+      cursado = {
+        id_turno: turnos[ii]['id'],
+        turno: turnos[ii]['numero'],
+        descripcion: turnos[ii]['descripcion'],
+        encargado: turnos[ii]['encargado'],
+        motivo: turnos[ii]['motivo'],
+
       }
-    
-    
+      todos.push([cursado])
+    }
+
+
 
   }
 
@@ -314,7 +314,7 @@ router.get('/detalledelcurso/:id', isLoggedInn2, async (req, res) => {
   const id = req.params.id
   try {
     /////CLASES DEL CURSO
- 
+
     //////pendientes inscriptos prioridad 1 2 3
     const pendientes1 = await pool.query('select inscripciones.id id_inscripcion,personas.hijos, dni_persona,inscripciones.horario, inscripciones.estado,inscripciones.uno,inscripciones.dos,inscripciones.tres, personas.nombre,personas.dni, personas.apellido, personas.trabajo, personas.tipo_trabajo, personas.participante_anterior from inscripciones join personas on inscripciones.dni_persona = personas.dni where uno=?  ', [id])
 
@@ -336,13 +336,14 @@ router.get('/detalledelcurso/:id', isLoggedInn2, async (req, res) => {
     ///////datos del curso
     const cursos = await pool.query('select * from cursos where id = ? ', [id])
     turnos = await pool.query('select * from turnos where id_curso = ? ', [id])
-    curso = [{nombre:cursos[0]['nombre'],
-    cupo:44*turnos.length,
-    turnos:turnos.length
-    
+    curso = [{
+      nombre: cursos[0]['nombre'],
+      cupo: 44 * turnos.length,
+      turnos: turnos.length
+
     }]
 
-    
+
     ////////////ALUMNOS YAINSCRIPTOS
     const inscriptos = await pool.query('select * from cursado where id_curso=? ', [id])
     console.log(cursado.length)
@@ -526,23 +527,23 @@ router.get('/detalledelcurso/:id', isLoggedInn2, async (req, res) => {
     res.json([array1, curso, inscriptos.length, clases, lista, cursado]);
   } catch (error) {
     console.log(error)
-    res.json([[''],[''],0,[''],[''],['']]);
+    res.json([[''], [''], 0, [''], [''], ['']]);
   }
 
 
 })
 
 
-router.post("/modificarcurso",isLoggedInn2,  async (req, res) => {
-  const { id, nombre  } = req.body
-try {
- 
-  await pool.query('update cursos set nombre=? where id=?  ', [nombre,id])
-  res.json('Realizado')
-} catch (error) {
-console.log(error)
-  res.json('No Realizado')
-}
+router.post("/modificarcurso", isLoggedInn2, async (req, res) => {
+  const { id, nombre } = req.body
+  try {
+
+    await pool.query('update cursos set nombre=? where id=?  ', [nombre, id])
+    res.json('Realizado')
+  } catch (error) {
+    console.log(error)
+    res.json('No Realizado')
+  }
 
 
 })
@@ -553,12 +554,12 @@ router.post("/crear", isLoggedInn2, async (req, res) => {
 
 
   try {
-    
-    fecha= (new Date(Date.now())).toLocaleDateString()
-    
 
-    await pool.query('insert cursos  set nombre=?, fecha=?', [nombre,fecha])
-   
+    fecha = (new Date(Date.now())).toLocaleDateString()
+
+
+    await pool.query('insert cursos  set nombre=?, fecha=?', [nombre, fecha])
+
 
     res.json('Realizado')
   } catch (error) {
@@ -575,7 +576,7 @@ router.post("/nuevoturno", isLoggedInn4, async (req, res) => {
   try {
 
 
-    
+
     await pool.query('insert turnos  set id_curso=?,numero=?,descripcion=?', [id_curso, numero, descripcion])
     res.json('Cargada nueva clase')
   } catch (error) {
@@ -598,7 +599,7 @@ router.post("/nuevaclase", isLoggedInn4, async (req, res) => {
     const nuev = {
       id_curso, fecha, observacion: observaciones
     }
-    await pool.query('insert clases  set id_curso=?,fecha=?,observacion=?', [id_curso, fecha,observaciones])
+    await pool.query('insert clases  set id_curso=?,fecha=?,observacion=?', [id_curso, fecha, observaciones])
     res.json('Cargada nueva clase')
   } catch (error) {
     console.log(error)
@@ -635,48 +636,48 @@ router.post("/inscribir", isLoggedInn, async (req, res) => {
 
 })
 
-router.post("/traerlosturnos",  async (req, res) => {
+router.post("/traerlosturnos", async (req, res) => {
   const { id } = req.body
   try {
     console.log(id)
-    turnos = await pool.query('select * from turnos where id_curso =?',[id])
+    turnos = await pool.query('select * from turnos where id_curso =?', [id])
     res.json(turnos)
   } catch (error) {
     console.log(error)
-    res.json ([{id:id}])
+    res.json([{ id: id }])
   }
-  
+
 
 
 })
 
 
-router.post("/presente",  async (req, res) => {
+router.post("/presente", async (req, res) => {
   const { id_alumno, asistencia, id_clase, observaciones } = req.body ///
 
-///asistencia (presente ausente)
-try {
+  ///asistencia (presente ausente)
+  try {
 
-console.log(id_alumno)
-console.log(id_clase)
-   const yatomada = await pool.query('select * from asistencia where id_persona = ? and id_clase =? ',[id_alumno,id_clase])
-if (yatomada.length>0){
-
-
+    console.log(id_alumno)
+    console.log(id_clase)
+    const yatomada = await pool.query('select * from asistencia where id_persona = ? and id_clase =? ', [id_alumno, id_clase])
+    if (yatomada.length > 0) {
 
 
-  await pool.query('update asistencia set asistencia=?, justificacion=? where id=?  ', [asistencia,observaciones,yatomada[0]['id']])
 
-}else{
 
-   await pool.query('insert into asistencia set id_persona=?,asistencia=?,id_clase=?,justificacion=? ', [id_alumno,asistencia,id_clase,observaciones])
+      await pool.query('update asistencia set asistencia=?, justificacion=? where id=?  ', [asistencia, observaciones, yatomada[0]['id']])
+
+    } else {
+
+      await pool.query('insert into asistencia set id_persona=?,asistencia=?,id_clase=?,justificacion=? ', [id_alumno, asistencia, id_clase, observaciones])
+    }
+    res.json('Realizado')
+
+  } catch (error) {
+    console.log(error)
+    res.json('Error algo sucedio')
   }
-   res.json('Realizado')
-
-} catch (error) {
-  console.log(error)
-  res.json('Error algo sucedio')
-}
 
 })
 
