82941cda4deff66d767bcefa126be66513936028
FGEGitHub <pipao.pipo@gmail.com>
2023-06-01 12:59:42 -0300

    ver escuela

--- src/routes/fiscalizacion.js
+++ src/routes/fiscalizacion.js
@@ -605,7 +605,7 @@ router.get('/confirmarcapa/:id', async (req, res,) => {
 router.get('/traerdetallesdeunaescuela/:id_escuela', async (req, res,) => {
     const { id_escuela } = req.params
 
-    const mesass = await pool.query('select * from mesas_fiscales left join ( select mesa as mesaasig, dni from asignaciones_fiscales) as selec1 on mesas_fiscales.id= selec1.mesaasig  join (select dni as dnipersona, nombre as nombrepers, apellido from personas_fiscalizacion) as selec2 on selec1.dni=selec2.dnipersona where id_escuela=? ', [id_escuela])
+    const mesass = await pool.query('select * from mesas_fiscales left join ( select mesa as mesaasig, dni from asignaciones_fiscales) as selec1 on mesas_fiscales.id= selec1.mesaasig  join (select dni as dnipersona, nombre as nombrepers, apellido, id_donde_vota from personas_fiscalizacion) as selec2 on selec1.dni=selec2.dnipersona join (select id as idesc, nombre as nombreesc from escuelas) as selec5 on selec2.id_donde_vota=selec5.idesc where id_escuela=? ', [id_escuela])
     console.log(mesass)
     res.json(mesass)
 
