023d6d458e5304636ef6f5d4dab902f11f8db81a
FGEGitHub <pipao.pipo@gmail.com>
2023-09-11 11:07:21 -0300

    inscrip dos

--- src/routes/inscripciones.js
+++ src/routes/inscripciones.js
@@ -641,7 +641,7 @@ router.post('/cargarexcelpersonas', async (req, res) => {
 router.get('/incriptas2da/', async (req, res) => {
 
 
-  inscriptos = await pool.query('select * from inscripciones join (select dni, nombre, apellido from personas) as sel on inscripciones.dni_persona=sel.dni join (select id as id1, nombre as nombrecurso1 from cursos) as sel2 on inscripciones.uno=sel2.id1 join (select id as id2, nombre as nombrecurso2 from cursos) as sel3 on inscripciones.uno=sel3.id2 where edicion=2')
+  inscriptos = await pool.query('select * from inscripciones join (select dni, nombre, apellido from personas) as sel on inscripciones.dni_persona=sel.dni join (select id as id1, nombre as nombrecurso1 from cursos) as sel2 on inscripciones.uno=sel2.id1 join (select id as id2, nombre as nombrecurso2 from cursos) as sel3 on inscripciones.dos=sel3.id2 where edicion=2')
   console.log(inscriptos)
   res.json([inscriptos])
 })
