b3d72fe19875fff289ac39f396683a78640fb1a6
FGEGitHub <pipao.pipo@gmail.com>
2023-09-13 21:55:23 -0300

    fsd

--- src/keys.js
+++ src/keys.js
@@ -2,10 +2,10 @@
 module.exports = {
     database:{
         host: '127.0.0.1',
-        //user: 'root',
-          user: 'admin',
-       // password: 'root',
-       password:'11235',
+        user: 'root',
+        //  user: 'admin',
+        password: 'root',
+      // password:'11235',
         database: 'marketing'
     }
 
