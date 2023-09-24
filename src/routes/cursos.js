4dffa8cf6ab353fcd5ccdb2e8a77b3562c07d747
FGEGitHub <pipao.pipo@gmail.com>
2023-09-24 18:35:00 -0300

    coordinadoras

--- src/routes/cursos.js
+++ src/routes/cursos.js
@@ -424,7 +424,7 @@ router.get('/borrarturno/:id', async (req, res) => {
 router.get('/listadetodoslosturnos/', isLoggedInn2, async (req, res) => {
   try {
     console.log('listadetodos')
-    tur = await pool.query('select * from turnos   join  (select id as idcurso, nombre as nombrecurso from cursos) as selec1  on turnos.id_curso= selec1.idcurso left join (select id as idu, nombre as encargado from usuarios) as selec2 on turnos.id_encargado=selec2.idu where etapa=2')
+    tur = await pool.query('select * from turnos   join  (select id as idcurso, nombre as nombrecurso from cursos) as selec1  on turnos.id_curso= selec1.idcurso left join (select id as idu, nombre as encargado from usuarios) as selec2 on turnos.id_encargado=selec2.idu  left join (select id as idu2, nombre as coordinador from usuarios) as selec3 on turnos.id_coordinador=selec3.idu2 where etapa=2')
 
 
  
