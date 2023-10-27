0b8ad5745c886fd6493a9093b5c568876769c0f6
FGEGitHub <pipao.pipo@gmail.com>
2023-10-27 18:07:43 -0300

    carnavales

--- src/routes/carnavales.js
+++ src/routes/carnavales.js
@@ -30,4 +30,56 @@ router.post("/desinscribir", async (req, res) => {
     }
 
 })
+
+
+
+router.get('/traerinscripcionesenc/', async (req, res) => {
+    const id = req.params.id
+  
+  
+    const etc = await pool.query('select * from inscripciones_carnaval left join (select dni as dnip, nombre, apellido from personas) as selec on inscripciones_carnaval.dni_persona=selec.dnip   where  (id_call is null or id_call= 0)  ')
+    console.log(etc)
+  
+    res.json(etc);
+  
+  
+  
+  
+  })
+  
+
+  router.post("/asignarinscripciones", async (req, res) => {
+    let { id, inscrip } = req.body
+  
+  
+    for (ins in inscrip) {
+  
+       
+  
+        await pool.query('update inscripciones_carnaval set id_call =?, estado="Preasignada"  where  id = ?', [id, inscrip[ins]])
+    }
+    res.json('realizado')
+  
+  })
+
+
+
+  router.get('/preinscriptascall/:id', async (req, res) => {
+    const id = req.params.id
+    
+      try {
+      
+        inscriptos = await pool.query('select * from inscripciones_carnaval join (select dni, nombre, apellido,categoria, participante_anterior, trabajo, hijos, tipo_trabajo,tel,tel2 from personas) as sel on inscripciones_carnaval.dni_persona=sel.dni  where id_call=? ',[id])
+     
+        res.json([inscriptos])
+        
+      } catch (error) {
+    console.log(error)
+        res.json(["error"]) 
+       }
+      })
+    
+
+
+
 module.exports = router
--- src/routes/index.js
+++ src/routes/index.js
@@ -23,6 +23,54 @@ try {
 //res.render('index')
 })
 
+
+
+router.get('/tllamadoscarnaval', async (req, res) => {
+ 
+ 
+  try {
+    const encargados = await pool.query('select * from usuarios where nivel =6')
+  
+    let envio = []
+    asignados = 0
+    for (encargado in encargados) {
+        let asignados = await pool.query('select * from inscripciones_carnaval where id_call =? ', [encargados[encargado]['id']])
+  
+  
+        let sinc = await pool.query('select * from inscripciones_carnaval where id_call =? and estado="Preasignada" ', [encargados[encargado]['id']])
+        let asig = await pool.query('select * from inscripciones_carnaval where id_call =? and estado="Asignada a curso" ', [encargados[encargado]['id']])
+        let rech = await pool.query('select * from inscripciones_carnaval where id_call =? and estado="Rechazada" ', [encargados[encargado]['id']])
+         nocont = await pool.query('select * from inscripciones_carnaval where id_call =? and estado="No contesta" ', [encargados[encargado]['id']])
+  
+        let objeto_nuevo = {
+            id: encargados[encargado]['id'],
+            nombre: encargados[encargado]['nombre'],
+            asignados: asignados.length,
+            sinc: sinc.length,
+            asig: asig.length,
+            rech: rech.length,
+            nocont:nocont.length
+  
+        }
+        envio.push(objeto_nuevo)
+    }
+  
+  
+    console.log(envio)
+    res.json([envio])
+  } catch (error) {
+    console.log(error)
+  res.json(['error'])
+  }
+   
+  
+  
+  
+  //res.render('index')
+  })
+
+
+
 router.get('/todoslosencargadosllamados', async (req, res) => {
  
  
