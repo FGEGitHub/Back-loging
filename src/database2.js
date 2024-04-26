8e79034c06ebb1ca74a197d6efaadfe5a300c834
FGEGitHub <pipao.pipo@gmail.com>
2024-04-26 11:14:03 -0300

    bd

---
+++ src/database2.js
@@ -0,0 +1,30 @@
+const mariadb = require('mariadb')
+
+const {database2} =require (('./keys'))
+const {promisify } = require('util')
+
+const pool = mariadb.createPool(database2)
+
+pool.getConnection((err,connection)=> {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('DATABASE CONNECTION WAS CLOSED')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR'){
+            console.error('DATABASE HAS TO MANY CONNECTIONS')  
+        }
+        if (err.code === ('ECONNREFUSED')){
+            console.error('DATABASE CONNECTION WAS REFUSED')
+        }
+        console.log(error)
+    }
+    if (connection) connection.release()
+    console.log('DB is Connected')
+    return;
+})
+//convirtiendo promesas
+//pool.query = promisify(pool.query)
+
+module.exports = pool
+
+
--- src/keys.js
+++ src/keys.js
@@ -2,11 +2,19 @@
 module.exports = {
     database:{
         host: '127.0.0.1',
-       // user: 'root',
-          user: 'admin',
-      //  password: 'root',
-       password:'11235',
+       user: 'root',
+      //    user: 'admin',
+        password: 'root',
+      // password:'11235',
         database: 'marketing'
-    }
+    },
+    database2:{
+      host: '127.0.0.1',
+     user: 'root',
+    //    user: 'admin',
+      password: 'root',
+    // password:'11235',
+      database: 'don_eulogio'
+  }
 
 }
