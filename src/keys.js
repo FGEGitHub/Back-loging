3fc4e226be93ba75f878af4e5c3847a783e73baa
FGEGitHub <pipao.pipo@gmail.com>
2024-04-26 11:17:56 -0300

    keys

--- src/keys.js
+++ src/keys.js
@@ -4,16 +4,16 @@ module.exports = {
         host: '127.0.0.1',
        user: 'root',
       //    user: 'admin',
-        password: 'root',
-      // password:'11235',
+    //    password: 'root',
+       password:'11235',
         database: 'marketing'
     },
     database2:{
       host: '127.0.0.1',
      user: 'root',
     //    user: 'admin',
-      password: 'root',
-    // password:'11235',
+     // password: 'root',
+     password:'11235',
       database: 'don_eulogio'
   }
 
