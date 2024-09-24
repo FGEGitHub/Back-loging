24ef13cda539309781fd2e934db30de32d0c54f4
FGEGitHub <pipao.pipo@gmail.com>
2024-09-24 14:57:56 -0300

    w

--- src/routes/turnos.js
+++ src/routes/turnos.js
@@ -318,7 +318,8 @@ router.get('/traerturnosparainscri/', async (req, res) => {
 
 router.get('/traerinscripcionesenc/:id', async (req, res) => {
 
-  const tu = await pool.query('select * from inscripciones join (select id as idp, nombre, apellido,categoria, participante_feria from personas) as sel on  inscripciones.id_persona=sel.idp  where edicion=4 and (estado="Inscripta" or estado="Rechazada") order by modalidad')
+
+  const tu = await pool.query('select * from inscripciones join (select id as idp, nombre, apellido,categoria, participante_feria from personas) as sel on  inscripciones.id_persona=sel.idp join (select id as idt, descripcion from turnos) as sel2 on inscripciones.uno=sel2.idt join (select id as idt2, descripcion as descripcion2 from turnos) as sel3 on inscripciones.dos=sel3.idt2 where edicion=5 and (estado="Inscripta" or estado="Rechazada") order by modalidad')
 
 
   res.json(tu)
