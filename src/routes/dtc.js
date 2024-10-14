11274e9436eb4b02a2a833afb3ffc3886b9c9612
FGEGitHub <pipao.pipo@gmail.com>
2024-10-14 17:17:01 -0300

    x<z

--- src/routes/dtc.js
+++ src/routes/dtc.js
@@ -1050,6 +1050,47 @@ router.post("/borrarinformeps", async (req, res) => {
   }
 });
 
+
+router.post("/borrarcosa", async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    // Obtener la ubicación del archivo desde la base de datos
+    const result = await pool.query('SELECT ubicacion FROM stc_cosas_usuario WHERE id = ?', [id]);
+
+    if (result.length > 0) {
+      const archivoUbicacion = result[0].ubicacion;
+
+      // Construir la ruta completa del archivo
+      let filePath = ''
+
+      try {
+         filePath = path.join(__dirname, '../imagenesvendedoras', archivoUbicacion);
+
+      } catch (error) {
+        
+      }
+
+      // Intentar eliminar el archivo del sistema de archivos si existe
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+        console.log('Archivo eliminado:', filePath);
+      } else {
+        console.log('El archivo no existe:', filePath);
+      }
+
+      // Eliminar la entrada de la base de datos
+      await pool.query('DELETE FROM dtc_asistencias_sociales WHERE id = ?', [id]);
+
+      res.json('Realizado');
+    } else {
+      res.json('No se encontró la entrada para el ID proporcionado');
+    }
+  } catch (error) {
+    console.error('Error al borrar la actividad social:', error);
+    res.json('No realizado');
+  }
+});
 router.post("/borraractividadsocial", async (req, res) => {
   const { id } = req.body;
 
