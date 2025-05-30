6acacc1a02d9c43db18cfda31f7189c540f6e34d
FGEGitHub <pipao.pipo@gmail.com>
2025-05-30 20:49:14 -0300

    inscrop

--- src/routes/personas.js
+++ src/routes/personas.js
@@ -1241,7 +1241,7 @@ router.post("/enviarinscripcioncarnaval", async (req, res) => {
 
     }
     pers = await pool.query('select * from personas where dni =?', [dni])
-    const yainsc = await pool.query('select * from inscripciones_carnaval where id_persona =? ', [pers[0]['id']])
+    const yainsc = await pool.query('select * from inscripciones_carnaval where id_persona =? and id>590', [pers[0]['id']])
     let mensaje = ''
     if (yainsc.length > 0) {
       mensaje = 'Con estos datos ya tenemos una inscripción, no hace falta que te vuelvas a anotar. Por favor aguarda nuestro contacto.'
