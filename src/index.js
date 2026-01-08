35aac198c9f573f9db1db629ed0f08292137eeaf
FGEGitHub <pipao.pipo@gmail.com>
2026-01-08 11:20:49 -0300

    as

--- src/index.js
+++ src/index.js
@@ -107,7 +107,7 @@ app.use(`/carnavales`, require('./routes/carnavales'))
 app.use(`/vendedoras`, require('./routes/vendedoras'))
 app.use(`/doneu`, require('./routes/doneu'))
 app.use(`/f1`, require('./routes/f1'))
-app.use(`/clinica`, require('./routes/clinica'))
+//app.use(`/clinica`, require('./routes/clinica'))
 //app.use(`/links`, require('./routes/links'))
 
 
